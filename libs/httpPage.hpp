96b2efe5edb269720aecc03997f4da50eb2138cf
jishan484 <jishan484@gmail.com>
2022-04-17 19:15:53 +0530

    stopping autoload of fevicon.iso icon in html page

--- libs/httpPage.hpp
+++ libs/httpPage.hpp
@@ -19,6 +19,7 @@ void parseHttpPage()
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="data:,">
     <title>PiWebVNC client</title>
 </head>
 <style>
