0621c83b7fc3ef507a0ea4bcdc2bead24746f4c2
jishan484 <jishan484@gmail.com>
2022-04-17 14:46:07 +0530

    fix BadRegion error : test 1

--- libs/vncserver.hpp
+++ libs/vncserver.hpp
@@ -23,7 +23,6 @@ class VNCServer
     private:
         Display * display;
         Damage damage;
-        XserverRegion xregion;
         ScreenInfo screenInfo;
         char config[500] = {0}; // 500byte not too much
         bool isRunning = true;
@@ -88,10 +87,10 @@ void VNCServer::start_service(Websocket &ws)
             else
             {
                 int partCounts = 0;
-                this->xregion = XFixesCreateRegion(this->display, 0, 0);
+                const XserverRegion xregion = XFixesCreateRegion(this->display, NULL, 0);
                 XDamageSubtract(this->display, this->damage, None, xregion);
-                XRectangle *rect = XFixesFetchRegion(this->display, this->xregion, &partCounts);
-                XFixesDestroyRegion(this->display, this->xregion);
+                XRectangle *rect = XFixesFetchRegion(this->display, xregion, &partCounts);
+                XFixesDestroyRegion(this->display, xregion);
                 for (int i = 0; i < partCounts; i++)
                 {
                     image = XGetImage(display, this->screenInfo.root, rect[i].x, rect[i].y, rect[i].width, rect[i].height, AllPlanes, ZPixmap);
