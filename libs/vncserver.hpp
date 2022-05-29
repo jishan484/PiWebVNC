628c8d3a32b5b21d2c960e0dc01a1b25f3ab756b
jishan484 <jishan484@gmail.com>
2022-05-29 13:20:40 +0530

    Fixed

--- libs/vncserver.hpp
+++ libs/vncserver.hpp
@@ -14,29 +14,29 @@
 
 class VNCServer
 {
-public:
-    XDisplay xdisplay;
-    VNCServer();
-    ~VNCServer();
-    void start_service(Websocket &ws);
-    void stop_service();
-    void send_first_frame(int sd); // send the first frame to the client
-private:
-    Display *display;
-    Damage damage;
-    ScreenInfo screenInfo;
-    char config[500] = {0}; // 500byte not too much
-    bool isRunning = true;
-    bool sendFirstFrame = false;
-    int clientSD = 0;
+    public:
+        XDisplay xdisplay;
+        VNCServer();
+        ~VNCServer();
+        void start_service(Websocket &ws);
+        void stop_service();
+        void send_first_frame(int sd); // send the first frame to the client
+    private:
+        Display * display;
+        Damage damage;
+        ScreenInfo screenInfo;
+        char config[500] = {0}; // 500byte not too much
+        bool isRunning = true;
+        bool sendFirstFrame = false;
+        int clientSD = 0;
 };
 
 VNCServer::VNCServer()
 {
-    int damage_event, damage_error, test;
+    int damage_event, damage_error, test;   
     this->display = xdisplay.getDisplay();
     this->screenInfo = xdisplay.getScreenInfo();
-    strcpy(config, xdisplay.getDisplayConfig().c_str());
+    strcpy(config,xdisplay.getDisplayConfig().c_str());
 
     test = XDamageQueryExtension(display, &damage_event, &damage_error);
     this->damage = XDamageCreate(display, this->screenInfo.root, XDamageReportNonEmpty);
@@ -64,17 +64,20 @@ void VNCServer::start_service(Websocket &ws)
     int sleepDelay = 1000000 / FPS;
     XImage *image;
     const XserverRegion xregion = XFixesCreateRegion(this->display, NULL, 0);
-    while (isRunning)
+    while(isRunning)
     {
         usleep(sleepDelay);
         if (ws.clients > 0)
         {
             if (this->sendFirstFrame)
             {
-                image = XGetImage(this->display, this->screenInfo.root, 0, 0, this->screenInfo.width, this->screenInfo.height, AllPlanes, ZPixmap);
+                image = XGetImage(this->display, this->screenInfo.root, 0, 0, this->screenInfo.width
+                    , this->screenInfo.height, AllPlanes, ZPixmap);
                 int frameSize = ((this->screenInfo.height - 1) * image->bytes_per_line + (this->screenInfo.width - 1));
                 int compressedSize = LZ4_compress_default(image->data, buffer, frameSize, bufferSize);
-                std::string data = "UPD" + std::to_string(0) + " " + std::to_string(0) + " " + std::to_string(this->screenInfo.width) + " " + std::to_string(this->screenInfo.height) + " " + std::to_string(image->bytes_per_line) + " " + std::to_string(compressedSize) + " \n";
+                std::string data = "UPD" + std::to_string(0) + " " + std::to_string(0) + " " 
+                    + std::to_string(this->screenInfo.width) + " " + std::to_string(this->screenInfo.height) + " " 
+                    + std::to_string(image->bytes_per_line) + " " + std::to_string(compressedSize) + " \n";
                 char *info = (char *)data.c_str();
                 int infoSize = strlen(info);
                 ws.sendText(config, this->clientSD);
@@ -96,7 +99,9 @@ void VNCServer::start_service(Websocket &ws)
                     image = XGetImage(display, this->screenInfo.root, rect[i].x, rect[i].y, rect[i].width, rect[i].height, AllPlanes, ZPixmap);
                     int frameSize = (rect[i].height * image->bytes_per_line);
                     int compressedSize = LZ4_compress_default(image->data, buffer, frameSize, bufferSize);
-                    std::string data = "UPD" + std::to_string(rect[i].x) + " " + std::to_string(rect[i].y) + " " + std::to_string(rect[i].width) + " " + std::to_string(rect[i].height) + " " + std::to_string(image->bytes_per_line) + " " + std::to_string(compressedSize) + " \n";
+                    std::string data = "UPD" + std::to_string(rect[i].x) + " " + std::to_string(rect[i].y) + " " 
+                        + std::to_string(rect[i].width) + " " + std::to_string(rect[i].height) + " " 
+                        + std::to_string(image->bytes_per_line) + " " + std::to_string(compressedSize) + " \n";
                     char *info = (char *)data.c_str();
                     int infoSize = strlen(info);
                     ws.sendFrame(info, buffer, infoSize, compressedSize);
