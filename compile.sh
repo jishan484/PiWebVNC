e32b728afa02501615b628677b44b49f158ef141
jishan484 <jishan484@gmail.com>
2022-06-29 18:07:53 +0530

    compile script updated

--- compile.sh
+++ compile.sh
@@ -1,9 +1,22 @@
 apt install -y libx11-dev libxdamage-dev libxfixes-dev libxtst-dev liblz4-dev g++
 
-g++ PIwebVNC.cpp -lX11 -lXdamage -lXfixes -pthread -lXtst -llz4 -o PiWebVNC
+g++ PIwebVNC.cpp -lX11 -lXdamage -lXfixes -pthread -lXtst -llz4 -o /bin/PiWebVNC
 
 echo "Compile Successful."
 
-echo "Running this app. using ./PiWebVNC"
+echo "Installing PiWebVNC...for autostart"
+echo "[Unit]
+Description=Remote desktop service (VNC)
 
-./PiWebVNC
+[Service]
+User="$(who|awk '{print $1}')"
+ExecStart=/bin/PiWebVNC
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/PiWebVNC.service
+
+sudo systemctl enable PiWebVNC.service
+sudo systemctl daemon-reload
+sudo systemctl start PiWebVNC.service
