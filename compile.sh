74d29025a371d0ad832af22356a05ef5f0e498f2
jishan484 <jishan484@gmail.com>
2022-06-30 23:00:31 +0530

    compile.sh Updated

--- compile.sh
+++ compile.sh
@@ -1,15 +1,30 @@
+# check if running as sudo
+if [ -z "$SUDO_USER" ]; then
+  echo "[ERROR] Please run as sudo, even if you are root."
+  echo "[INFO] Try: sudo sh compile.sh"
+  echo "[EXIT] Exiting..."
+  exit
+fi
+
 apt install -y libx11-dev libxdamage-dev libxfixes-dev libxtst-dev liblz4-dev g++
 
 g++ PIwebVNC.cpp -lX11 -lXdamage -lXfixes -pthread -lXtst -llz4 -o /bin/PiWebVNC
 
-echo "Compile Successful."
+echo "[INFO] Compile Successful."
 
-echo "Installing PiWebVNC...for autostart"
+echo "[INFO] Installing PiWebVNC...for autostart"
+
+if [ -z "$SUDO_USER" ]; then
+    PIwebVNC_USER=$USER
+else
+    PIwebVNC_USER=$SUDO_USER
+fi
+
 echo "[Unit]
 Description=Remote desktop service (VNC)
 
 [Service]
-User="$(who|awk '{print $1}')"
+User="$PIwebVNC_USER"
 ExecStart=/bin/PiWebVNC
 Restart=always
 RestartSec=10
