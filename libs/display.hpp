1ac65cd701f4e4f8b3e94a5ccacbc7fd6301dc65
jishan484 <jishan484@gmail.com>
2024-03-23 00:39:20 +0530

    xvfb support (issue pi4 no screen found)

--- libs/display.hpp
+++ libs/display.hpp
@@ -28,6 +28,7 @@
 #include <X11/extensions/Xfixes.h>
 #include <string>
 #include "websocket.hpp"
+#include "xvfb_support.hpp"
 
 struct ScreenInfo
 {
@@ -55,7 +56,7 @@ private:
 
 XDisplay::XDisplay()
 {
-    // get result of command "echo $DISPLAY"
+    //get result of command "echo $DISPLAY"
     try
     {
         std::string envDisplay = std::string(getenv("DISPLAY"));
@@ -79,8 +80,11 @@ XDisplay::XDisplay()
     {
         #if ERROR || DEBUG
             std::cout << "[ERROR][EXIT APP] Could not open display. Please pass --display [id].\n\t eg: --display 18." << std::endl;
-        #endif
-        exit(1);
+            std::cout << " or " << std::endl;
+            installXvfb();
+            initXvfb();
+#endif
+                exit(1);
     }
     #if ERROR || DEBUG
         std::cout << "[LOG] Display opened successfully." << std::endl;
@@ -166,3 +170,12 @@ void XDisplay::close()
      * otherwise it will loop though all displays and find the one that is running
      * loop limit is set to 10 change from config file (config.cpp)
      */
+
+    /*
+    * if display is not set then it will try to start Xvfb
+    * and then start the display manager
+    * it will try to start the following desktop environments
+    * gnome, mate, cinnamon, pantheon, lxqt, kde, xfce, lxsession, enlightenment
+    * if any of the above desktop environment is installed then it will start that
+    * otherwise it will exit the app
+    */
